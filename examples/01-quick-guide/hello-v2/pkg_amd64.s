8597f700e571ab12621202b967fdcda57cd0323b
chai2010 <chaishushan@gmail.com>
2018-05-06 14:56:03 +0800

    zz

--- examples/01-quick-guide/hello-v2/pkg_amd64.s
+++ examples/01-quick-guide/hello-v2/pkg_amd64.s
@@ -15,12 +15,6 @@ type eface struct {
 	data unsafe.Pointer
 }
 
-type _type struct {
-	name string
-	bits uint
-	signed bool
-}
-
 type itab struct {
 	inter *interfacetype
 	_type *_type
@@ -28,6 +22,37 @@ type itab struct {
 	_     [4]byte
 	fun   [1]uintptr // variable sized. fun[0]==0 means _type does not implement inter.
 }
+
+type interfacetype struct { // 80 bytes on a 64bit arch
+	typ     _type
+	pkgpath name
+	mhdr    []imethod
+}
+
+type imethod struct {
+	name nameOff
+	ityp typeOff
+}
+
+type _type struct { // 48 bytes on a 64bit arch
+	size       uintptr
+	ptrdata    uintptr // size of memory prefix holding all pointers
+	hash       uint32
+	tflag      tflag
+	align      uint8
+	fieldalign uint8
+	kind       uint8
+	alg        *typeAlg
+	// gcdata stores the GC type data for the garbage collector.
+	// If the KindGCProg bit is set in kind, gcdata is a GC program.
+	// Otherwise it is a ptrmask bitmap. See mbitmap.go for details.
+	gcdata    *byte
+	str       nameOff
+	ptrToThis typeOff
+}
+
+func resolveNameOff(ptrInModule unsafe.Pointer, off nameOff) name {}
+func resolveTypeOff(ptrInModule unsafe.Pointer, off typeOff) *_type {}
 */
 
 TEXT ·HelloWorld(SB),$56-0
