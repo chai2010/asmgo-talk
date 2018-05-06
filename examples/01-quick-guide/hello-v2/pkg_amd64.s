c532a8dcc10c9e42789448c3a1d049d581ae2ba5
chai2010 <chaishushan@gmail.com>
2018-05-06 11:50:59 +0800

    zz

--- examples/01-quick-guide/hello-v2/pkg_amd64.s
+++ examples/01-quick-guide/hello-v2/pkg_amd64.s
@@ -32,24 +32,24 @@ type itab struct {
 
 TEXT ·HelloWorld(SB),$56-0
 	// var a0 interface{} = helloworld
-	// MOVQ $0, a0_type-16(SP) // a0._type
-	// MOVQ $0, a0_data-8(SP)  // a0.data
+	// MOVQ $0, a0_type-8*2(SP) // a0._type
+	// MOVQ $0, a0_data-8*1(SP) // a0.data
 
 	// func runtime.convT2E(t *_type, elem unsafe.Pointer) (e eface)
-	LEAQ type·string(SB),   AX; MOVQ AX, 0(SP) // arg:t: string type
-	LEAQ ·helloworld+0(SB), BX; MOVQ BX, 8(SP) // arg:elem: &helloworld
+	LEAQ type·string(SB), AX; MOVQ AX, 8*0(SP) // arg:t: string type
+	LEAQ ·helloworld(SB), BX; MOVQ BX, 8*1(SP) // arg:elem: &helloworld
 	CALL runtime·convT2E(SB)
-	MOVQ 16(SP), AX;  MOVQ AX, a0_type-16(SP)  // ret0._type
-	MOVQ 24(SP), BX;  MOVQ BX, a0_data-8(SP)   // ret0.data
+	MOVQ 8*2(SP), AX; MOVQ AX, a0_type-8*2(SP) // ret:e._type
+	MOVQ 8*3(SP), BX; MOVQ BX, a0_data-8*1(SP) // ret:e.data
 
 	// func fmt.Println(a ...interface{}) (n int, err error)
-	LEAQ a0_type-16(SP), AX; MOVQ AX, 0(SP)    // arg:a.Data
-	MOVQ $1, 8(SP)                             // arg:a.Len
-	MOVQ $1, 16(SP)                            // arg:a.Cap
+	LEAQ a0_type-8*2(SP), AX; MOVQ AX, 8*0(SP) // arg:a.Data
+	MOVQ $1, 8*1(SP)                           // arg:a.Len
+	MOVQ $1, 8*2(SP)                           // arg:a.Cap
 	CALL fmt·Println(SB)                       // ret: ignore
 
-	// MOVQ 24(SP), AX                         // ret0:n
-	// MOVQ 32(SP), BX                         // ret1:err._type
-	// MOVQ 40(SP), CX                         // ret1:err.data
+	// MOVQ 8*3(SP), AX                        // ret:n
+	// MOVQ 8*4(SP), BX                        // ret:err._type
+	// MOVQ 8*5(SP), CX                        // ret:err.data
 
 	RET
