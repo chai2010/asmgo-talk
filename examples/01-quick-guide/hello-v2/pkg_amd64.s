db511f48b5957ef6255557792e600bbc4ad01ebb
chai2010 <chaishushan@gmail.com>
2018-05-06 08:40:11 +0800

    zz

--- examples/01-quick-guide/hello-v2/pkg_amd64.s
+++ examples/01-quick-guide/hello-v2/pkg_amd64.s
@@ -30,17 +30,26 @@ type itab struct {
 }
 */
 
-TEXT ·HelloWorld(SB),$48-0
+TEXT ·HelloWorld(SB),$56-0
+	// var a0 interface{} = helloworld
+	MOVQ $0, a0_type-16(SP) // a0._type
+	MOVQ $0, a0_data-8(SP)  // a0.data
+
 	// func runtime.convT2E(t *_type, elem unsafe.Pointer) (e eface)
 	LEAQ type·string(SB),   AX; MOVQ AX, 0(SP) // arg:t: string type
 	LEAQ ·helloworld+0(SB), AX; MOVQ AX, 8(SP) // arg:elem: &helloworld
 	CALL runtime·convT2E(SB)
-	MOVQ 16(SP), AX;                           // ret:e {_type, data}
+	MOVQ 16(SP), AX;  MOVQ AX, a0_type-16(SP)  // ret0._type
+	MOVQ 24(SP), AX;  MOVQ AX, a0_data-8(SP)   // ret0.data
 
 	// func fmt.Println(a ...interface{}) (n int, err error)
-	MOVQ AX, 0(SP)       // arg:a.Data
-	MOVQ $1, 8(SP)       // arg:a.Len
-	MOVQ $1, 16(SP)      // arg:a.Cap
-	CALL fmt·Println(SB) // ret: ignore
+	LEAQ a0_type-16(SP), AX; MOVQ AX, 0(SP)    // arg:a.Data
+	MOVQ $1, 8(SP)                             // arg:a.Len
+	MOVQ $1, 16(SP)                            // arg:a.Cap
+	CALL fmt·Println(SB)                       // ret: ignore
+
+	// MOVQ 24(SP), AX                         // ret0:n
+	// MOVQ 32(SP), AX                         // ret1:err._type
+	// MOVQ 40(SP), AX                         // ret1:err.data
 
 	RET
