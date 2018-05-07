4b1cf8dc80dba74deb84a13ee3b5f56540d29940
chai2010 <chaishushan@gmail.com>
2018-05-07 10:09:09 +0800

    zz

--- examples/01-quick-guide/hello-v2/main.go
+++ examples/01-quick-guide/hello-v2/main.go
@@ -4,15 +4,29 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
 	HelloWorld()
-	println(helloworld)
+	fmt.Println(helloworld)
+
+	fmt.Println("ptr0:", uintptr(unsafe.Pointer(&helloworld)))
+
+	//fmt.Println("ptr:", helloworld.Ptr())
 
 	// asm lex: (*)
 	// https://zh.wikipedia.org/wiki/%E6%98%9F%E8%99%9F
 	// https://zh.wikipedia.org/wiki/%E6%8B%AC%E5%8F%B7
+	//
+	// http://xahlee.info/comp/unicode_full-width_chars.html
+	//
+	// cmd/link/internal/objfile/objfile.go
+	// readSymName reads a symbol name, replacing all "". with pkg.
+	// func (r *objReader) readSymName() string
+	//
 	// 2217
 	for i, r := range "（∗*）（）" {
 		fmt.Printf("%d: 0x%X, %d\n", i, int(r), int(r))
