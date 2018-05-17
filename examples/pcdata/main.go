4e72d99f331fd47341c2a9b148bfa98c7bd141ce
chai2010 <chaishushan@gmail.com>
2018-05-17 18:10:08 +0800

    zz

--- examples/pcdata/main.go
+++ examples/pcdata/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main()
+/*
+"".main0.args_stackmap SRODATA size=8
+        0x0000 01 00 00 00 00 00 00 00                          ........
+"".main1.args_stackmap SRODATA size=9
+        0x0000 01 00 00 00 02 00 00 00 00                       .........
+"".main2.args_stackmap SRODATA size=9
+        0x0000 01 00 00 00 04 00 00 00 00                       .........
+"".main3.args_stackmap SRODATA size=9
+        0x0000 01 00 00 00 06 00 00 00 00                       .........
+"".main4.args_stackmap SRODATA size=9
+        0x0000 01 00 00 00 08 00 00 00 00                       .........
+"".main5.args_stackmap SRODATA size=10
+        0x0000 01 00 00 00 0a 00 00 00 10 00                    ..........
+*/
+
+func main0()
+func main1(a int)
+func main2(a, b int)
+func main3(a, b, c int)
+func main4(a, b, c, d int)
+func main5(a, b, c, d int, e *int)
+
+func main() {
+	main2(1, 2)
+}
--- examples/pcdata/main_amd64.s
+++ examples/pcdata/main_amd64.s
@@ -5,16 +5,23 @@
 #include "textflag.h"
 #include "funcdata.h"
 
-// "Hello World!\n"
-DATA  text<>+0(SB)/8,$"Hello Wo"
-DATA  text<>+8(SB)/8,$"rld!\n"
-GLOBL text<>(SB),NOPTR,$16
+TEXT ·main0(SB), $0-0
+	RET
+
+TEXT ·main1(SB), $0-8
+	RET
+TEXT ·main3(SB), $0-24
+	RET
+TEXT ·main4(SB), $0-32
+	RET
+TEXT ·main5(SB), $0-40
+	RET
 
 // func main()
-TEXT ·main(SB), $16-0
+TEXT ·main2(SB), $16-16
 	NO_LOCAL_POINTERS
 	MOVQ $1, (SP)
-	//PCDATA $0, $0
+	PCDATA $0, $0
 	CALL ·PrintCallerName(SB)
 	RET
 
