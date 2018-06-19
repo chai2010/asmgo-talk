b8f747f25b4defec50703eead0574fb19326f8b7
chai2010 <chaishushan@gmail.com>
2018-06-19 08:21:37 +0800

    zz

---
+++ examples/goid/pkg.go
@@ -0,0 +1,100 @@
+// Copyright 2018 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// https://github.com/golang/go/blob/master/src/runtime/runtime2.go
+// type g struct { ... }
+
+package main
+
+import (
+	"fmt"
+	"time"
+	"unsafe"
+)
+
+func getg() interface{}
+
+func getg_type() unsafe.Pointer
+func getg_addr() unsafe.Pointer
+
+func main() {
+	//go func() {
+	//	fmt.Println("-------------")
+	//	fmt.Printf("other goid: %0X\n", GetGoID())
+	//	fmt.Println("-------------")
+	//
+	//	fmt.Printf("main getg: %v\n", getg())
+	//}()
+
+	fmt.Println("-------------")
+	fmt.Printf("main goid: %0X\n", GetGoID())
+
+	fmt.Println("-------------")
+	fmt.Printf("main getg_type: %0X\n", getg_type())
+	fmt.Printf("main getg_addr: %0X\n", getg_addr())
+
+	p := (*g)(getg_addr())
+	fmt.Printf("getg_addr goid offsetof: %d, %d\n", unsafe.Offsetof(p.goid), offsetDict["go1.10"])
+
+	fmt.Printf("getg_addr v.id: %0X\n", p.goid)
+
+	//fmt.Printf("main getg: %v\n", getg())
+
+	time.Sleep(1)
+}
+
+type stack struct {
+	lo uintptr
+	hi uintptr
+}
+
+type g struct {
+	// Stack parameters.
+	// stack describes the actual stack memory: [stack.lo, stack.hi).
+	// stackguard0 is the stack pointer compared in the Go stack growth prologue.
+	// It is stack.lo+StackGuard normally, but can be StackPreempt to trigger a preemption.
+	// stackguard1 is the stack pointer compared in the C stack growth prologue.
+	// It is stack.lo+StackGuard on g0 and gsignal stacks.
+	// It is ~0 on other goroutine stacks, to trigger a call to morestackc (and crash).
+	stack       stack   // offset known to runtime/cgo
+	stackguard0 uintptr // offset known to liblink
+	stackguard1 uintptr // offset known to liblink
+
+	_panic       unsafe.Pointer // innermost panic - offset known to liblink
+	_defer       unsafe.Pointer // innermost defer
+	m            unsafe.Pointer // current m; offset known to arm liblink
+	sched        gobuf
+	syscallsp    uintptr        // if status==Gsyscall, syscallsp = sched.sp to use during gc
+	syscallpc    uintptr        // if status==Gsyscall, syscallpc = sched.pc to use during gc
+	stktopsp     uintptr        // expected sp at top of stack, to check in traceback
+	param        unsafe.Pointer // passed parameter on wakeup
+	atomicstatus uint32
+	stackLock    uint32 // sigprof/scang lock; TODO: fold in to atomicstatus
+	goid         int64
+}
+
+type gobuf struct {
+	// The offsets of sp, pc, and g are known to (hard-coded in) libmach.
+	//
+	// ctxt is unusual with respect to GC: it may be a
+	// heap-allocated funcval, so GC needs to track it, but it
+	// needs to be set and cleared from assembly, where it's
+	// difficult to have write barriers. However, ctxt is really a
+	// saved, live register, and we only ever exchange it between
+	// the real register and the gobuf. Hence, we treat it as a
+	// root during stack scanning, which means assembly that saves
+	// and restores it doesn't need write barriers. It's still
+	// typed as a pointer so that any other writes from Go get
+	// write barriers.
+	sp   uintptr
+	pc   uintptr
+	g    guintptr
+	ctxt unsafe.Pointer
+	ret  sys_Uintreg
+	lr   uintptr
+	bp   uintptr // for GOEXPERIMENT=framepointer
+}
+
+type guintptr uintptr
+type sys_Uintreg uint64
---
+++ examples/goid/pkg_amd64.s
@@ -0,0 +1,56 @@
+// Copyright 2018 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "textflag.h"
+#include "go_tls.h"
+
+// func GetGoID() int64
+TEXT ·GetGoID(SB), NOSPLIT, $0-8
+	get_tls(CX)
+	MOVQ g(CX), AX
+	MOVQ ·offset(SB), BX
+	LEAQ 0(AX)(BX*1), DX
+	MOVQ (DX), AX
+	MOVQ AX, ret+0(FP)
+	RET
+
+TEXT ·getg_type(SB), NOSPLIT, $8
+	LEAQ type·runtime·g(SB), AX
+	MOVQ AX, ret+0(FP)
+	RET
+
+TEXT ·getg_addr(SB), NOSPLIT, $8
+	get_tls(CX);
+	MOVQ g(CX), AX
+	MOVQ AX, ret+0(FP)
+	RET
+
+TEXT ·getg(SB),$32
+
+L_START:
+	MOVQ TLS, CX
+	MOVQ 0(CX)(TLS*1), AX
+	CMPQ SP, 16(AX)
+	JLS  L_MORE_STK
+
+	// get runtime.g
+	get_tls(CX);
+	MOVQ g(CX), AX
+
+	// get runtime.g type
+	LEAQ type·runtime·g(SB), BX;
+
+
+
+	MOVQ BX, 8*0(SP)                              // runtime.g type
+	MOVQ AX, 8*1(SP)                              // runtime.g data
+	CALL runtime·convT2E(SB)
+	MOVQ 8*2(SP), AX; MOVQ AX, ret_type-8*0(FP)   // ret:e._type
+	MOVQ 8*3(SP), BX; MOVQ BX, ret_data-8*1(FP)   // ret:e.data
+
+	RET
+
+L_MORE_STK:
+	CALL runtime·morestack_noctxt(SB)
+	JMP  L_START
