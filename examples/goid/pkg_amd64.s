3d90109c9f58dd183850b1e7ce79ece17eb3ee34
chai2010 <chaishushan@gmail.com>
2018-06-20 07:58:17 +0800

    zz

--- examples/goid/pkg_amd64.s
+++ examples/goid/pkg_amd64.s
@@ -24,8 +24,8 @@ TEXT ·getg_type(SB), NOSPLIT, $0-8
 
 // func getg_addr() unsafe.Pointer
 TEXT ·getg_addr(SB), NOSPLIT, $0-8
-	get_tls(CX)
-	MOVQ g(CX), AX
+	MOVQ TLS, CX
+	MOVQ 0(CX)(TLS*1), AX
 	MOVQ AX, ret+0(FP)
 	RET
 
@@ -35,16 +35,16 @@ TEXT ·getg(SB), NOSPLIT, $32-16
 	MOVQ $0, ret+8(FP)
 	GO_RESULTS_INITIALIZED
 
-	// get runtime.g type
-	MOVQ $type·runtime·g(SB), AX
-
 	// get runtime.g
-	get_tls(CX);
-	MOVQ g(CX), BX;
+	MOVQ TLS, CX
+	MOVQ 0(CX)(TLS*1), AX
+
+	// get runtime.g type
+	MOVQ $type·runtime·g(SB), BX
 
 	// convert (*g) to interface{}
-	MOVQ AX, 0(SP)
-	MOVQ BX, 8(SP)
+	MOVQ AX, 8(SP)
+	MOVQ BX, 0(SP)
 	CALL runtime·convT2E(SB)
 	MOVQ 16(SP), AX
 	MOVQ 24(SP), BX
